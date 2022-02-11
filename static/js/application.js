2a62f7640a6060fbbe585c60ade9f7ff8e7e8e74
atoly100 <99232653+atoly100@users.noreply.github.com>
2022-02-10 21:18:03 -0500

    Update application.js

--- static/js/application.js
+++ static/js/application.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function(){
     //connect to the socket server.
-    console.log("connect to"  + document.domain + "/test");
-    var socket = io.connect('https://' + document.domain + ':33507/test');
+    console.log("connect to"  + document.domain + ":" + document.port + "/test");
+    var socket = io.connect('https://' + document.domain + ':' + document.port + '/test');
     //var numbers_received = [];
     var num1 = 0;
     var num2 = 0;
