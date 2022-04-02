5e9b2b5d8f6df388a189ba09df715d686425688a
mahalakshmi <mllakshmi261@gmail.com>
2022-04-02 09:31:09 -0400

    chart changes

--- static/js/application.js
+++ static/js/application.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
         console.log("chart_redraw called");
 
         // set the chart title
-        chart.title("Population by Race for the United States: 2010 Census");
+        chart.title("Accumulated Volume (in mL");
 
         // add the data
         var data = [
