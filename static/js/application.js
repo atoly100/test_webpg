a287a78924869bab1247950aee6a7aa4d8df41a0
mahalakshmi <mllakshmi261@gmail.com>
2022-04-14 19:30:48 -0400

    change to 6 numbers

--- static/js/application.js
+++ static/js/application.js
@@ -6,6 +6,9 @@ $(document).ready(function(){
     var num1 = 0;
     var num2 = 0;
     var num3 = 0;
+    var num4 = 0;
+    var num5 = 0;
+    var num6 = 0;
 
     // create the chart
     var chart = anychart.pie();
@@ -21,6 +24,9 @@ $(document).ready(function(){
             { x: "Sensor 1", value: num1 },
             { x: "Sensor 2", value: num2 },
             { x: "Sensor 3", value: num3 },
+            { x: "Sensor 4", value: num4 },
+            { x: "Sensor 5", value: num5 },
+            { x: "Sensor 6", value: num6 },
         ];
         chart.data(data);
 
@@ -33,16 +39,22 @@ $(document).ready(function(){
         chart.container('chart');
         chart.draw();
 
-        var numbers = [ num1, num2, num3 ].sort().reverse();
+        var numbers = [ num1, num2, num3, num4, num5, num6 ].sort().reverse();
         get_label = function(n) {
             if (n == num1) return "Sensor 1: " + n;
             if (n == num2) return "Sensor 2: " + n;
             if (n == num3) return "Sensor 3: " + n;
+            if (n == num4) return "Sensor 4: " + n;
+            if (n == num5) return "Sensor 5: " + n;
+            if (n == num6) return "Sensor 6: " + n;
         }
 
         $("#id_01").text(get_label(numbers[0]));
         $("#id_02").text(get_label(numbers[1]));
         $("#id_03").text(get_label(numbers[2]));
+        $("#id_04").text(get_label(numbers[3]));
+        $("#id_05").text(get_label(numbers[4]));
+        $("#id_06").text(get_label(numbers[5]));
     }
 
     // redraw chart every 5 seconds
@@ -66,4 +78,23 @@ $(document).ready(function(){
         num3 = msg.number;
         $('#log3').attr("placeholder", "Sensor 3: " + num3).blur();
     });
+
+    socket.on('newnumber4', function(msg) {
+        console.log("Received number 4" + msg);
+        num4 = msg.number;
+        $('#log4').attr("placeholder", "Sensor 4: " + num4).blur();
+    });
+
+    socket.on('newnumber5', function(msg) {
+        console.log("Received number 5" + msg);
+        num5 = msg.number;
+        $('#log5').attr("placeholder", "Sensor 5: " + num5).blur();
+    });
+
+    socket.on('newnumber6', function(msg) {
+        console.log("Received number 6" + msg);
+        num6 = msg.number;
+        $('#log6').attr("placeholder", "Sensor 6: " + num6).blur();
+    });
+
 });
